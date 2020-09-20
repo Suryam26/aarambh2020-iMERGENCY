18719f470e6694492cc2ccaf7766d9df689102eb
Suryam26 <suryam.jain@gmail.com>
2020-09-20 22:19:04 +0530

    Enabled storing personal info

--- aarambh/imergency/static/js/script.js
+++ aarambh/imergency/static/js/script.js
@@ -1,7 +1,45 @@
 var url = $('#url').html();
+var name = '';
+var phn1 = '';
+var phn2 = '';
+var phn3 = '';
+
+
+if (localStorage.getItem("i_Name")) {
+  $("#name").attr("value", localStorage.getItem("i_Name"));
+}
+
+if (localStorage.getItem("i_phn1")) {
+  $("#phone1").attr("value", localStorage.getItem("i_phn1"));
+}
+
+if (localStorage.getItem("i_phn2")) {
+  $("#phone2").attr("value", localStorage.getItem("i_phn2"));
+}
+
+if (localStorage.getItem("i_phn3")) {
+  $("#phone3").attr("value", localStorage.getItem("i_phn3"));
+}
+
+
+function save() {
+  if (typeof(Storage) !== "undefined") {
+    name = $("#name").val();
+    phn1 = $("#phone1").val();
+    phn2 = $("#phone2").val();
+    phn3 = $("#phone3").val();
+    localStorage.setItem("i_Name", name);
+    localStorage.setItem("i_phn1", phn1);
+    localStorage.setItem("i_phn2", phn2);
+    localStorage.setItem("i_phn3", phn3);
+  }
+  else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+  }
+}
+
 
 function emergency() {
-
   if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
         var pos = {
@@ -12,9 +50,7 @@ function emergency() {
         window.open( link, '_self' );
     });
   }
-
   else {
     console.error("Geolocation is not supported by this browser.");
   }
-
 }
